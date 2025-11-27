3a1a750c586f5b68b831a9040155c395dd8f4844
Amatex1 <uncsnephews4@hotmail.com>
2025-11-27 11:36:52 +1030

    modified:   index.html modified:   src/components/Footer.css modified:   src/components/MiniChat.css modified:   src/components/Navbar.css modified:   src/index.css modified:   src/main.jsx modified:   src/pages/Feed.css modified:   src/pages/Messages.css new file:   src/styles/autoResponsive.css

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import './styles/darkMode.css'
 import './styles/responsive.css'
+import './styles/autoResponsive.css' // Auto-detect and adapt to all device sizes
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
